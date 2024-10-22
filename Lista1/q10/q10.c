30b934f3ad0b1abdce6363bfcd08b66c7c17c0c3
Daniel Braga <101554437+DanielBR0612@users.noreply.github.com>
2024-10-22 07:07:39 -0300

    Update q10.c

--- Lista1/q10/q10.c
+++ Lista1/q10/q10.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
 int main(){
+    int peso;
+    int g1;
+    int g2;
+    int t;
 
+    printf("Digite o peso do saco em KG: \n");
+    scanf("%d", &peso);
 
-    return;
+    printf("Digite a quantidade do Primeiro gato: \n");
+    scanf("%d", &g1);
+
+    printf("Digite a quantidade do segundo gato: \n");
+    scanf("%d", &g2);
+
+    peso = peso * 1000;
+    t = (g1 + g2) * 5;
+    t = peso - t;
+
+    printf("Resto da racao apos 5 dias: %d\n", t);
+
+    return 0;
 }
