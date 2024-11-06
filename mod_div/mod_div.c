443538a61446c64ad3b2ccf546d03b5f242687ad
Daniel <danielfranciscobragadearujo@gmail.com>
2024-11-06 16:20:11 -0300

    mod_div

---
+++ mod_div/mod_div.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int mod(int a, int b){
+    if (a < b){
+        return a;
+    }
+    else{
+        return mod(a - b, b);
+    }
+}
+
+int div(int a, int b, int q){
+    if (a < b){
+        return q;
+    }
+    else{
+        div(a - b, b, q += 1);
+    }
+}
+
+int main(){
+    int m = mod(10,4);
+    int d = div(10,1,0);
+
+    printf("%d", d);
+
+    return 0;
+}
